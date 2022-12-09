59b0a40b52fb22f334b6377b0c2199cc6398f881
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2022-12-09 18:30:56 -0300

    09/12
    sexta pós derrota do brasil na copa

--- Exercicios/saudacao.js
+++ Exercicios/saudacao.js
@@ -1,4 +1,13 @@
 (function () {
-    const nameUsu = "Dc";
-    document.getElementById("#top-bar .Welcome").textContent = "Boas Vindas Daniel";    
+    const nomeusuario = "Daniel";
+   // document.querySelector(".Welcome").innerHTML ="Bem vindo (Sr.), " + nomeusuario;
+    const elemento = document.querySelector(".top-bar p")
+    //troca todo o texto
+//    elemento.textContent = "Bem vindo (Sr), " + nomeusuario
+    //apenas adiciona a variavel
+//    elemento.textContent= elemento.textContent + nomeusuario
+    //mesma coisa porém resumido
+//    elemento.textContent += nomeusuario    
+    //mesma coisa porém com a possibilidade de modificar via html
+    elemento.innerHTML += "<b>" + nomeusuario + "<b>";
 })()
