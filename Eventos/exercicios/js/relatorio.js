1d6d55750570b61f1a57119bf89af6d13e3a0a9f
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2023-03-14 21:02:01 -0300

    14/03

--- Eventos/exercicios/js/relatorio.js
+++ Eventos/exercicios/js/relatorio.js
@@ -1,21 +1,43 @@
 'use strict';
+(function(){
+
+const contadorContainer = document.getElementById('contador');
 const titulo = document.getElementById('txtTitulo');
 const descricao = document.getElementById('txtDescricao');
 const spamCaracteres = document.getElementById('span');
 const button = document.getElementById('chkAceito');
 const submit  = document.getElementById('btn');
 const formCadastro = document.querySelector(".formCadastro");
+const resta = contadorContainer.getElementsByTagName('span')[0]
+//não coloquei o numero maximo para não precisar modificar depois
+const max = descricao.maxLength
 
-formCadastro
+formCadastro.addEventListener('submit', function(e){
 
-
-submit.addEventListener("click",function(e){  
-        console.log(titulo.value);
         if(!titulo.value){
             e.preventDefault;
             alert("Preencha todos os campos");
             titulo.focus();
 
         }
-
 })
+
+contadorContainer.removeAttribute('style')
+//outra forma
+//contadorContainer.style.display = 'block'
+
+descricao.addEventListener("keyup", function(){
+    console.log('keyup');
+})
+descricao.addEventListener("keydown", function(){
+    console.log('keydown');
+})
+descricao.addEventListener("keypress", function(){
+    console.log("keypress");
+})
+descricao.addEventListener('input', function(){
+    console.log("input");
+})
+})()
+
+
